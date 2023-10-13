def30f45b0153040f450bc90e63a7e0f6a33c086
Alexandr <alexandr.bosenko@gmail.com>
2023-10-13 21:57:45 +0300

    populate_db.sql fixed

--- sql/populate_db.sql
+++ sql/populate_db.sql
@@ -30,7 +30,7 @@ VALUES
     ('Avengers: Infinity War', 3, '2017-01-23', '2018-04-27'), -- id 7
     ('Avengers: Age of Ultron', 5, '2014-02-11', '2015-05-01'), -- id 8
     ('Guardians of the Galaxy Vol. 3', 4, '2021-11-08', '2023-05-05'), -- id 9
-    ('Captain America: The Winter Soldier', 4, '20213-04-01', '2014-04-04'); -- id 10
+    ('Captain America: The Winter Soldier', 4, '2013-04-01', '2014-04-04'); -- id 10
 
 INSERT INTO project_worker (project_id, worker_id)
 VALUES
